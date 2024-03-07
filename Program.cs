9dc74abe7fff736b2697b85deeecce1c5890b5e5
AslanbekHasanov <learneraslanbek1220@gmail.com>
2024-03-07 20:31:22 +0500

    Read All Car

--- Program.cs
+++ Program.cs
@@ -2,3 +2,17 @@
 // Copyright (c) Coalition of the Good-Hearted Engineers
 // FREE TO USE TO CONNECT THE WORLD
 // ---------------------------------------------------------------
+
+using Car.Crud.Broker.FileBroker;
+using Car.Crud.Broker.FileStorageBroker;
+using Car.Crud.Models.Cars;
+
+IFileStorageBroker storageBroker = new FileStorageBroker();
+
+List<ACar> cars = storageBroker.ReadALlCars();
+
+foreach(var car in cars )
+{
+    Console.WriteLine($"{car.Id}. {car.Name} - {car.Color} - {car.Number}");
+
+}
